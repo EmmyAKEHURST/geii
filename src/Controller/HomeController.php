da76c6c560b93a391c48278d982fe3a56dab60ac
Zohir Tamda <zohir@area17.com>
2026-04-14 13:30:31 +0200

    Add documenttion on controllers and methods.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,8 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+/**
+ * Handles public entry routes for the website.
+ */
 final class HomeController extends AbstractController
 {
+    /**
+     * Renders the website home page.
+     */
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
