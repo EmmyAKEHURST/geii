a579e3dbd35bc8a6c13d620e6d8fc3dc190361b5
Zohir Tamda <zohir@area17.com>
2026-04-14 11:29:54 +0200

    Improve the form type for the authentication.

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,11 +6,13 @@ use App\Entity\Compte;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
@@ -25,22 +27,51 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue(
-                        message: 'You should agree to our terms.',
+                        message: 'Vous devez accepter les conditions d\'utilisation.',
                     ),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Veuillez confirmer le mot de passe',
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
                 'constraints' => [
                     new NotBlank(
-                        message: 'Please enter a password',
+                        message: 'Veuillez saisir un mot de passe.',
                     ),
                     new Length(
-                        min: 6,
+                        min: 13,
                         max: 4096,
                         // max length allowed by Symfony for security reasons
-                        minMessage: 'Your password should be at least {{ limit }} characters',
+                        minMessage: 'Le mot de passe doit contenir au moins {{ limit }} caractères.',
+                    ),
+                    new Regex(
+                        pattern: '/[0-9]/',
+                        message: 'Le mot de passe doit contenir au moins un chiffre.',
+                    ),
+                    new Regex(
+                        pattern: '/[A-Z]/',
+                        message: 'Le mot de passe doit contenir au moins une lettre majuscule.',
+                    ),
+                    new Regex(
+                        pattern: '/[a-z]/',
+                        message: 'Le mot de passe doit contenir au moins une lettre minuscule.',
+                    ),
+                    new Regex(
+                        pattern: '/[^A-Za-z0-9]/',
+                        message: 'Le mot de passe doit contenir au moins un caractère spécial.',
                     ),
                 ],
             ])
