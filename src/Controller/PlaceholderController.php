545f3bb78b61c68070fa16d37003cd8057ff0fa2
Zohir Tamda <tamda.zohir.pro@gmail.com>
2026-05-17 15:02:22 +0200

    Tri du namespace pour PlaceholderController.php

--- src/Controller/PlaceholderController.php
+++ src/Controller/PlaceholderController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * Routes référencées par le template d'accueil
