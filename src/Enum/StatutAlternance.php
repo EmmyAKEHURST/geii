fc81e1e27d8e75597537cb769ea20d74df75e8a3
Zohir Tamda <tamda.zohir.pro@gmail.com>
2026-05-18 21:07:33 +0200

    Documentation des enums.

--- src/Enum/StatutAlternance.php
+++ src/Enum/StatutAlternance.php
@@ -2,6 +2,12 @@
 
 namespace App\Enum;
 
+/**
+ * Enumération de statut d'alternance.
+ * ACTIVE : l'offre d'alternance est disponible.
+ * POURVUE : l'offre d'alternance est occupée.
+ * EXPIREE : l'offre d'alternance n'est plus disponible.
+ */
 enum StatutAlternance: string
 {
     case ACTIVE = 'active';
--- src/Enum/StatutProjetTuteure.php
+++ src/Enum/StatutProjetTuteure.php
@@ -2,6 +2,13 @@
 
 namespace App\Enum;
 
+/**
+ * Énumère les statuts des projets tuteurés.
+ *
+ * OUVERT : le projet tuteuré est disponible.
+ * EN_COURS : le projet tuteuré est en cours de travail.
+ * TERMINE : le projet tuteure est finalisé.
+ */
 enum StatutProjetTuteure: string
 {
     case OUVERT = 'ouvert';
