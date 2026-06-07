6f2626a30fa0f2ef70e53551a8453f02eb74835b
Zohir Tamda <tamda.zohir.pro@gmail.com>
2026-06-07 13:37:23 +0200

    Adapter la redirection de la route après succès de l'authentification.

---
+++ src/EventListener/LoginSuccessListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
+
+#[AsEventListener]
+readonly class LoginSuccessListener
+{
+    public function __construct(
+        private RouterInterface $router
+    ) {}
+
+    public function __invoke(LoginSuccessEvent $event): void
+    {
+        $user = $event->getAuthenticatedToken()->getUser();
+
+        $roleRouteMap = [
+            'ROLE_PERSONNEL'  => 'app_espace_personnel_index',
+            'ROLE_ENSEIGNANT' => 'app_espace_enseignant_index',
+            'ROLE_ENTREPRISE' => 'app_espace_entreprise_index',
+            'ROLE_ETUDIANT'   => 'app_espace_etudiant_index',
+        ];
+
+        foreach ($roleRouteMap as $role => $route) {
+            if (in_array($role, $user->getRoles(), true)) {
+                $event->setResponse(new RedirectResponse($this->router->generate($route)));
+
+                return;
+            }
+        }
+    }
+}
